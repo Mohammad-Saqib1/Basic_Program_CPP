1b02ba38f298022f0c43af97f2c553ac34a446b7
Mohammad-Saqib1 <87147700+Mohammad-Saqib1@users.noreply.github.com>
2021-07-11 20:28:37 +0530

    Add files via upload

---
+++ print_AtoZ.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+//ASCII CODE 65 to 90 FOR A to Z
+void AToZ()
+{
+    char ch = 90;
+    for (int i = 65; i <= ch; i++)
+    {
+        cout << char(i) << " ";
+    }
+}
+int main()
+{
+    AToZ();
+    return 0;
+}
